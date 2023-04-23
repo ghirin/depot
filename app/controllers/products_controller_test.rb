bd1ca638c9278139948bed54df1e09438f0397b5
Yury Ghirin <yury.ghirin@gmail.com>
2023-04-23 16:21:37 +0300

    Добавлены тесты и настроено кеширование

--- app/controllers/products_controller_test.rb
+++ app/controllers/products_controller_test.rb
@@ -3,33 +3,35 @@ class ProductsControllerTest < ActionController::TestCase
 setup do
 @product = products(:one)
     @update = {
-    title: 'Lorem Ipsum',
-    description: 'Wibbles are fun!',
-    image_url: 'lorem.jpg',
-    price: 19.95
+        title: 'Lorem Ipsum',
+        description: 'Wibbles are fun!',
+        image_url: 'lorem.jpg',
+        price: 19.95
     }
 end
-test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:products)
-end
-test "should get new" do
-    get :new
-    assert_response :success
-end
 
-test "should create product" do
-    assert_difference('Product.count') do
-    post :create, product: @update
-end
+    test "should get index" do
+        get :index
+        assert_response :success
+        assert_not_nil assigns(:products)
+    end
 
-assert_redirected_to product_path(assigns(:product))
-end
-# ...
-test "should update product" do
-    patch :update, id: @product, product: @update
+    test "should get new" do
+        get :new
+        assert_response :success
+    end
+
+    test "should create product" do
+        assert_difference('Product.count') do
+        post products_url, params: { product: @update }
+    end
+
     assert_redirected_to product_path(assigns(:product))
-end
-# ...
+    end
+
+    test "should update product" do
+        patch product_url(@product), params: { product: @update }
+        assert_redirected_to product_path(assigns(:product))
+    end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,7 @@ class Product < ApplicationRecord
             with: %r{\.(gif|jpg|png)\Z}i,
             message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
         }
+    def self.latest
+      Product.order(:updated_at).last
+    end
 end
